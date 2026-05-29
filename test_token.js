251713099a57378457618b4fa411cd325e3cc32c
chydroid <chydroid@163.com>
2026-05-29 17:18:05 +0800

    chore: 删除临时测试脚本、无关文件和包含敏感信息的文件（31个文件）

--- test_token.js
+++
@@ -1,3 +0,0 @@
-const jwt = require("./packages/gateway/node_modules/jsonwebtoken");
-const token = jwt.sign({ userId: 'test', roles: ['user'] }, 'EvoClaw-dev-secret-key-2026', { expiresIn: '24h' });
-console.log(token);
